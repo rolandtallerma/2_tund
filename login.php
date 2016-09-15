fc802821864aafea279216ace551d60ac6e17925
rolatall <rolatall@greeny.cs.tlu.ee>
2016-09-15 12:44:37 +0300

    updated login page

--- login.php
+++ login.php
@@ -1,28 +1,71 @@
+<?php
+
+
+	//var_dump($_GET);
+	//echo "<br>";
+	//var_dump($_POST);
+	
+	$signupEmailError = "";
+	
+	//kas on üldse olemas?
+	if(isset ($_POST["signupEmail"])) {
+		
+		//oli olemas, ehk keegi vajutas nuppu
+		if (empty($_POST["signupEmail"])) {
+			
+			//oli tõesti tühi
+			$signupEmailError = "See väli on kohustuslik";
+		
+		}
+	
+	}
+?>
+
 <!DOCTYPE html>
+
 <html>
 	<head>
-		<title>Sisselogimise leht</title>
+		<title>Log in page</title>
 	</head>
 	<body>
 
-		<h1>Logi sisse</h1>
+		<h1>Log in</h1>
 		
-		<form>
+		<form method="POST">
 		
 		
-		<input type="email">
+		<input placeholder="E-mail" name="loginEmail" type="email">
 		
 		<br><br> 
 		
 		
-		<input type="password">
+		<input placeholder="Password" name="loginPassword" type="password">
 		
 		<br><br>
-		<input type="submit">
+		<input type="submit" value="Log in">
 		
 		
 		
 	</form>
-
+	
+	<h1>Create user</h1>
+		
+		<form method="POST">
+				
+			<input placeholder="E-mail" name="signupEmail" type="email"> <?php echo $signupEmailError; ?>
+		
+			<br><br> 
+			
+			<input placeholder="Password" name="signupPassword" type="password">
+		
+			<br><br>
+			
+			<input type="submit" value="Create user">
+		
+		
+		
+		</form>
+	
 	</body>
 </html>
+
